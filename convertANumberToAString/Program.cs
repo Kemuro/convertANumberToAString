9e63e066db11f62ed34e8dac2ef929109503ba9e
Kemuro <antonkemuro@gmail.com>
2021-04-02 16:33:42 -0500

    initial commit

---
+++ convertANumberToAString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace convertANumberToAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* We need a function that can transform a number into a string.
+             * What ways of achieving this do you know?
+             * 
+             * Examples:
+             * 123 --> "123"
+             * 999 --> "999"  */
+
+        }
+        public class Kata
+        {
+            public static string NumberToString(int num)
+            {
+                //code here
+                string numToString = num.ToString();
+
+                return numToString;
+
+                //passed codewars test
+            }
+        }
+    }
+}
